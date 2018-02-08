bdcb0a17810a5cef8d68989c9d553f66060a71e2
gamgine <victor.fromentin95@ynov.com>
2018-02-08 17:58:29 +0000

    html semantique

---
+++ session.php
@@ -0,0 +1,5 @@
+<?php
+	$icon = $img="/asset/icon.jpg";
+	$css=array("/asset/style.css");
+	$designer=$author="gamgine";
+	include("view/session.php");
---
+++ view/header.php
@@ -0,0 +1,15 @@
+		<header>
+		</header>
+		<nav>
+			<ul>
+				<li><a href="/"><img src="" alt="logo"></a></li>
+				<li><a href="/">openshop</a></li>
+			</ul>
+			<ul>
+				<li><a href="/">h</a></li>
+				<li><a href="/articles.php">articles</a></li>
+			</ul>
+			<ul>
+				<li><a href="/session.php">login register</a></li>
+			</ul>
+		</nav>
