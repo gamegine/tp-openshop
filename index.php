315729c9600fa2c8122ed2f079d5a4eb25b036ef
root <root@212-83-172-83.nitrogroup.fr>
2018-02-11 13:47:09 +0000

    css hader,nav,footer

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	$site_name="openshop";
 	$icon ="/asset/icon.png";
-	$css=array("/asset/style.css","https://use.fontawesome.com/releases/v5.0.6/css/all.css");
+	$css=array("/asset/style.css","https://use.fontawesome.com/releases/v5.0.6/css/all.css","https://unpkg.com/purecss@1.0.0/build/pure-min.css");
 	$designer=$author="gamgine";
 	$img = "/asset/banniere.jpg";
 
--- view/header.php
+++ view/header.php
@@ -1,16 +1,16 @@
 		<header>
 			<img src="/asset/banniere.jpg" alt="banniere">
 		</header>
-		<nav>
-			<ul>
-				<li><a href="/"><img src="openshop logo" alt="logo"></a></li>
-				<li><a href="/">openshop</a></li>
+		<nav class="pure-g">
+			<ul class="pure-u-2-3">
+				<li class="ib" id="logo"><a href="/"><img src="<?=$icon?>" alt="logo" width="100%"></a></li>
+				<li class="ib"><a href="/">openshop</a></li>
 			</ul>
-			<ul>
-				<li><a href="/">h</a></li>
-				<li><a href="/articles.php">articles</a></li>
+			<ul class="pure-u-1-3">
+				<li class="ib"><a href="/session.php">login register</a></li>
 			</ul>
-			<ul>
-				<li><a href="/session.php">login register</a></li>
+			<ul class="pure-u-1-1">
+				<li class="ib"><a href="/">h</a></li>
+				<li class="ib"><a href="/articles.php">articles</a></li>
 			</ul>
 		</nav>
