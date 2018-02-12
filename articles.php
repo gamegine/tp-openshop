5924370048bd580f9245f650b9aea17791afad5b
root <root@212-83-172-83.nitrogroup.fr>
2018-02-12 10:08:51 +0000

    css r v1

--- articles.php
+++ articles.php
@@ -2,7 +2,9 @@
 	$site_name="openshop";
 	$icon ="/asset/icon.png";
 	$css=array("/asset/style.css");
-	$css=array("https://use.fontawesome.com/releases/v5.0.6/css/all.css","https://unpkg.com/purecss@1.0.0/build/pure-min.css","/asset/style.css");
+	$css=array("https://use.fontawesome.com/releases/v5.0.6/css/all.css",
+	"https://unpkg.com/purecss@1.0.0/build/pure-min.css",
+	"https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css","/asset/style.css",);
 	$designer=$author="gamgine";
 	$img = "/asset/banniere.jpg";
 
