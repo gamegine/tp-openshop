ad88341ca8ce8953d0ac02a41a554f5fc45a84cf
root <root@212-83-172-83.nitrogroup.fr>
2018-02-13 17:36:41 +0000

    bdd data select+nav session

--- article.php
+++ article.php
@@ -1,14 +1,29 @@
 <?php
+	session_start();
 	$site_name="openshop";
 	$icon ="/asset/icon.png";
 	$css=array("https://use.fontawesome.com/releases/v5.0.6/css/all.css","https://unpkg.com/purecss@1.0.0/build/pure-min.css","/asset/style.css");
 
 	$designer=$author="gamgine";
 	
+	include("asset/bdd.php");
 	$arimg=array("/articlesimg/arimg.jpg");
-	$article=array('name'=>"article name",'sdesc'=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",'prix'=>250,'desc'=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>Cum, tempore, amet temporibus unde deleniti quos recusandae architecto nostrum facere voluptas nam corporis mollitia nesciunt atque repellat incidunt repudiandae pariatur deserunt?</span><span>Deleniti, voluptatem corporis dicta vel perferendis. Voluptatibus, reprehenderit quibusdam in cum blanditiis necessitatibus id ullam ab. Minima, fugiat, quibusdam repellat quaerat veniam dolores repellendus hic vero! Quam aliquid impedit quos.",'img'=>$arimg);
 	
+	$aid=intval(htmlentities($_GET['id']));
+	$reponse = $bdd->prepare('SELECT `articles`.* FROM `articles` WHERE `articles`.`id`=:id');
+	$reponse->bindValue(':id',$aid,PDO::PARAM_INT);
+	$reponse->execute();
+	$donnees = $reponse->fetch();
+	$article=array('name'=>$donnees['name'],'sdesc'=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",'prix'=>$donnees['prix'],'delivery'=>$donnees['delivery'],'desc'=>$donnees['txt'],'img'=>array());
+	$reponse->closeCursor();
 	
+	$reponse = $bdd->prepare('SELECT `img`.url FROM `img` WHERE `img`.`aid`=:aid');
+	$reponse->bindValue(':aid',$aid,PDO::PARAM_INT);
+	$reponse->execute();
+	$donnees = $reponse->fetch();
+	{ array_push($article['img'],"/articlesimg/".$donnees['url'] ); }
+	$reponse->closeCursor();
+
 	$img = $arimg[0];
 	$title=$article['name'];
 	$desc=$article['sdesc'];
