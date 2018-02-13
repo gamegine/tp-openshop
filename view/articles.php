279b44b4b819ed7c5e4b776c7a5fb1d142f3e7c7
gamgine <victor.fromentin95@ynov.com>
2018-02-13 23:43:16 +0000

    action.php+cart echo+php+view

--- view/articles.php
+++ view/articles.php
@@ -6,13 +6,12 @@
 <?php include('header.php'); ?>
 		<main class="pure-g">
 			<div class="pure-u-20-24 pure-g " id="articles">
-				<?php
-foreach($articles as $article)
+<?php foreach($articles as $article)
 {
 echo '				<article class="pure-u-sm-1-1 pure-u-md-10-24 pure-u-lg-1-4 pure-u-xl-4-24 dc">
 					<a href="'.$article['url'].'" class="dc"></a><img src='.$article['img'].' alt="ar img" width="100%">
 					<h2>'.$article['name'].'</h2><p>'.$article['sdesc'].'</p><p>'.$article['prix'].' $<p>
-					<p class="link"><a href="'.$article['url'].'">see the offer</a> <a href="#">buy now</a></p>
+					<p class="link"><a href="'.$article['url'].'">see the offer</a> <a href="'.$article['action'].'">buy now</a></p>
 				</article>';
 }?>
 				<div>
---
+++ view/cart.php
@@ -0,0 +1,42 @@
+<?php $url="/cart.php"; ?>
+<!doctype html>
+<html lang="<?=$lang="en"?>">
+<?php include('asset/head.php'); ?>
+	<body>
+<?php include('header.php'); ?>
+		<main>
+			<table class="cart pure-table pure-table-horizontal">
+				<caption>cart</caption>
+				
+				<thead>
+					<tr>
+						<th>img</th>
+						<th>article</th>
+						<th>prix</th>
+						<th>action</th>
+					</tr>
+				</thead>
+				<!--tfoot>
+					<tr>
+						<th>img</th>
+						<th>article</th>
+						<th>prix</th>
+						<th>action</th>
+					</tr>
+				</tfoot-->
+				<tbody>
+<?php foreach($articles as $article)
+{
+echo '					<tr>
+						<td><img src='.$article['img'].' alt="ar img" width="100%"></td>
+						<td><h3>'.$article['name'].'</h3><p>'.$article['sdesc'].'</p></td>
+						<td>'.$article['prix'].'</td>
+						<td><a href="'.$article['action'].'">delete</a></td>
+					</tr>';
+}?>
+				</tbody>
+			</table>
+		</main>
+<?php include('footer.html'); ?>	
+	</body>
+</html>
