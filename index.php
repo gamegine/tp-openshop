b133e40b0299e075d8dc532f21e192ddce504e09
root <root@212-83-172-83.nitrogroup.fr>
2018-02-14 14:28:16 +0000

    articles pages

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 	$reponse->execute();
 	$articles=array();
 	while ($donnees = $reponse->fetch())
-	{array_push($articles, array('name'=>$donnees['name'],'prix'=>$donnees['prix'],'img'=>"/articlesimg/".$donnees['url'],'url'=>"/article.php?id=".$donnees['id'],'action'=>"action.php?a=a&id=".$donnees['id']) );}
+	{array_push($articles, array('name'=>$donnees['name'],'prix'=>$donnees['prix'],'img'=>"/articlesimg/".$donnees['url'],'url'=>"/article.php?id=".$donnees['id'],'action'=>"action.php?a=b&id=".$donnees['id']) );}
 	$reponse->closeCursor();
 
 	$title="openshop index";
