34e0efffa0d5651ebb1247724267c84d3ad613d7
gamgine <victor.fromentin95@ynov.com>
2018-02-15 23:04:35 +0000

    cart v2

--- cart.php
+++ cart.php
@@ -14,14 +14,18 @@
 		$img = "/asset/banniere.jpg";
 		$title="openshop cart";
 		$desc="open shop e-commerce dédié à l'open source";
-		
-		$reponse = $bdd->prepare('SELECT `cart`.`id` as cartid,`img`.`url`,`articles`.*,`cart`.`pay`,`cart`.`completed` FROM `cart`,`articles`,`img` WHERE `img`.`id`=ifnull(`articles`.`img`,0) and `articles`.`id`=`cart`.`aid` and `cart`.`uid`=:id');
+
+		$reponse = $bdd->prepare('SELECT `cart`.`id` as cartid,`img`.`url`,`articles`.*,`cart`.`pay`,`cart`.`completed` FROM `cart`,`articles`,`img` WHERE `img`.`id`=ifnull(`articles`.`img`,0) and `articles`.`id`=`cart`.`aid` and `cart`.`uid`=:id order by `cart`.`pay`');
 		$reponse->bindValue(':id',$_SESSION['id'],PDO::PARAM_INT);
 		$reponse->execute();
 		$articles=array();
+		$articlesold=array();
 		$total=0;
 		while ($donnees = $reponse->fetch())
-		{$total+=intval($donnees['prix']);array_push($articles, array('name'=>htmlentities($donnees['name']),'sdesc'=>htmlentities(substr($donnees['txt'],0,124)),'prix'=>$donnees['prix'],'img'=>"/articlesimg/".$donnees['url'],'action'=>"/action/delete.html/id=".$donnees['cartid']) );}
+		{
+			if(!$donnees['completed']){if(!$donnees['pay']){$total+=intval($donnees['prix']);}array_push($articles, array('name'=>htmlentities($donnees['name']),'sdesc'=>htmlentities(substr($donnees['txt'],0,124)),'prix'=>$donnees['prix'],'img'=>"/articlesimg/".$donnees['url'],'action'=>"/action/delete.html/id=".$donnees['cartid'],'state'=>$donnees['pay']));}
+			else{array_push($articlesold, array('name'=>htmlentities($donnees['name']),'sdesc'=>htmlentities(substr($donnees['txt'],0,124)),'prix'=>$donnees['prix'],'img'=>"/articlesimg/".$donnees['url'],'action'=>"/action/buy.html/id=".$donnees['id']) );}
+		}
 		$reponse->closeCursor();
 		
 		include("view/cart.php");
